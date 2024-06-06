61f58ebb08931f33a656e575b5d7e2d5a8e9e34c
Jose <mpjosemanuel86@gmail.com>
2024-06-06 10:30:47 +0200

    Fragment info SmartSolar implementado

---
+++ app/src/main/java/com/example/practicafct/ui/fragments/DialogFragment.kt
@@ -0,0 +1,38 @@
+package com.example.practicafct.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+import androidx.fragment.app.DialogFragment
+import com.example.practicafct.R
+
+class MyDialogFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_informacion, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val buttonOk = view.findViewById<Button>(R.id.button_ok)
+
+        buttonOk.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+    }
+}
--- app/src/main/java/com/example/practicafct/ui/fragments/PantallaDetallesFragment.kt
+++ app/src/main/java/com/example/practicafct/ui/fragments/PantallaDetallesFragment.kt
@@ -8,33 +8,42 @@ import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import com.example.practicafct.R
 import com.example.practicafct.databinding.FragmentPantallaDetallesBinding
+import com.example.practicafct.ui.fragments.MyDialogFragment
 import com.example.practicafct.ui.viewmodel.DatosSmartSolarViewModel
-
 
 class PantallaDetallesFragment : Fragment() {
 
     private lateinit var binding: FragmentPantallaDetallesBinding
     private val viewModel: DatosSmartSolarViewModel by activityViewModels()
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         binding = FragmentPantallaDetallesBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         viewModel.energyDataLiveData.observe(viewLifecycleOwner) { energyData ->
             binding.etCAU.setText(energyData.cau)
             binding.etEstado.setText(energyData.estadoSolicitud)
             binding.etTipo.setText(energyData.tipoAutoconsumo)
             binding.etCompensacion.setText(energyData.compensacionExcedentes)
             binding.etPotencia.setText(energyData.potenciaInstalacion)
+        }
+
+        // Obtén una referencia al ImageButton que desencadenará la apertura del diálogo
+        val btnInfo = binding.ibInfo
+
+        // Configura el clic del ImageButton para abrir el diálogo
+        btnInfo.setOnClickListener {
+            // Crea una instancia del fragmento del diálogo
+            val dialogFragment = MyDialogFragment()
+            // Muestra el diálogo
+            dialogFragment.show(parentFragmentManager, "MyDialogFragment")
         }
     }
 }
